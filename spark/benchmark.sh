87b3b7e691a5a7f59abed9bda8e60a0f0225d0a0
DoubleMindy <doublemind21@gmail.com>
2023-09-20 00:39:36 +0300

    Update benchmark.sh

--- spark/benchmark.sh
+++ spark/benchmark.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install openjdk-8-jdk-headless
 # mv spark-3.0.1-bin-hadoop2.7 spark
 
 wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
-#gzip -d hits.tsv.gz
+gzip -d hits.tsv.gz
 chmod 777 ~ hits.tsv
 $HADOOP_HOME/bin/hdfs dfs -put hits.tsv /
 
